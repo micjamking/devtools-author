9b86d99c07f63053d2ee95fd331a0fb919770c29
Mike King <yo@mikeking.io>
2015-09-14 01:03:31 -1000

    Fixing JShint issues

--- app/scripts/panel.js
+++ app/scripts/panel.js
@@ -208,8 +208,8 @@
           panel.themes = JSON.parse(ajax.responseText);
 
           panel.themes.sort(function(a, b){
-            if(a.name < b.name) return -1;
-            if(a.name > b.name) return 1;
+            if(a.name < b.name) { return -1; }
+            if(a.name > b.name) { return 1; }
             return 0;
           });
           
