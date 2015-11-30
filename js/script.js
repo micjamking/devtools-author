b0a608d75d6708e1eadf1ded20d84bbdc7826f88
Mike King <yo@mikeking.io>
2015-11-29 22:47:10 -1000

    Added social links

--- js/script.js
+++ js/script.js
@@ -8,22 +8,29 @@
   var date = new Date();
   year.innerHTML = date.getFullYear();
 
-//   var ajax = new XMLHttpRequest();
-//   ajax.open('GET', 'https://api.github.com/repos/micjamking/devtools-author?access_token=1ca9eca028e56648d0cb35c5e4693b6b836fa614');
-//   ajax.send(null);
+  function documentReady(){
+    var links = $('.share-links')[0];
+    links.style.display = 'block';
+  }
 
-//   ajax.onreadystatechange = function(){
-//     if (ajax.readyState === 4) {
-//       if (ajax.status === 200) {
-//         if (ajax.responseText) {
-//           var github = JSON.parse(ajax.responseText);
-//           $('.stargazers')[0].innerHTML = github.watchers + ' stargazers';
-//         }
-//       } else {
-//         console.log('Status Code: ' + ajax.status + '\nThere was an error with your request');
-//       }
-//     }
-//   };
+  window.addEventListener('load', documentReady);
+
+  var ajax = new XMLHttpRequest();
+  //ajax.open('GET', 'https://api.github.com/repos/micjamking/devtools-author?access_token=aec7d69b1d0123a8ea309431ec4a4150dcc1961d');
+  //ajax.send(null);
+
+  ajax.onreadystatechange = function(){
+    if (ajax.readyState === 4) {
+      if (ajax.status === 200) {
+        if (ajax.responseText) {
+          var github = JSON.parse(ajax.responseText);
+          $('.stargazers')[0].innerHTML = github.watchers + ' GitHub Stargazers';
+        }
+      } else {
+        console.log('Status Code: ' + ajax.status + '\nThere was an error with your request');
+      }
+    }
+  };
 })(
 document.querySelectorAll.bind(document),
 window
