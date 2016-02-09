5c84c7061519a8aee19ac039a1060af789d69bfe
Bennie Mosher <benniemosher@gmail.com>
2016-02-09 16:23:21 -0700

    Uncommented file that includes all JS files

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      // 'app/**/*.js',
+      'app/**/*.js',
       'test/**/*-spec.js'
     ],
 
