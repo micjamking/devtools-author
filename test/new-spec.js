c4377fd1692f0ac04682678fbb9c49a553a78996
Mike King <yo@mikeking.io>
2016-02-15 13:29:14 -1000

    Adding simple tests for chrome.* API; addresses #19

--- test/new-spec.js
+++ test/new-spec.js
@@ -11,4 +11,14 @@ describe('a new spec', function ()
   {
     expect(a).toEqual(1);
   });
+
+  it ('chrome.storage API is defined', function()
+  {
+    expect(chrome.storage).toBeDefined();
+  });
+
+  it ('chrome.devtools API is defined', function()
+  {
+    expect(chrome.devtools).toBeDefined();
+  });
 });
